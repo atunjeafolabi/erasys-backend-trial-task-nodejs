88927725b3e5a00645a5d2b2d92d761c1a47ab62
Atunje <atunje_afolabi@yahoo.com>
2021-09-06 20:54:30 +0100

    initial commit

---
+++ src/server.ts
@@ -0,0 +1,13 @@
+import express from 'express';
+import "reflect-metadata";
+
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+    res.send('The sedulous hyena ate the antelope!');
+});
+
+app.listen(port, () => {
+    console.log(`server is listening on ${port}`);
+});
