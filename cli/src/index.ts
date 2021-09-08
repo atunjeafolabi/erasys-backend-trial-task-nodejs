f589b55ed2603b377b1b27afdf112cc9838531f0
Atunje <atunje_afolabi@yahoo.com>
2021-09-08 08:22:16 +0100

    cli project setup

---
+++ cli/src/index.ts
@@ -0,0 +1,10 @@
+#! /usr/bin/env node
+const clear = require('clear');
+
+clear();
+
+console.log('Hello World!');
+
+// console.log(
+//   chalk.red(figlet.textSync('pizza-cli', { horizontalLayout: 'full' }))
+// );
